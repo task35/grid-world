6c8a8ac3e5848916a6564a8656d30fd7772a17eb
timsgardner <timsgardner@gmail.com>
2017-05-19 21:25:21 -0400

    ehh

--- src/task35/grid_world/magic_perf.clj
+++ src/task35/grid_world/magic_perf.clj
@@ -18,10 +18,12 @@
 (defn move-towards [obj1, obj2, speed]
   (with-cmpt obj1 [tr1 Transform]
     (with-cmpt obj2 [tr2 Transform]
-      (identity
-       ;;m/faster
+      (;;identity
+       m/faster
         (let [diff (v3- (.position tr2) (.position tr1))]
           (set! (.position tr1)
             (v3+ (.position tr1)
                  (v3* (.normalized diff)
                       (Mathf/Min speed (.magnitude diff))))))))))
+
+
